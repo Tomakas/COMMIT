5b6de1ef7c345309f220dd79e98e298984f5fb0d
Tomas <kvasnica.tomas@seznam.cz>
2025-06-28 23:37:52 +0200

    fix: update localization keys in navigation buttons and user profile display

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -12,13 +12,13 @@
 
   <template v-if="display.lgAndUp.value && navInAppBar">
     <div class="app-bar-left max-width-100">
-      <v-btn to="/dashboard" prepend-icon="mdi-view-dashboard">{{ navWithText ? $t("nav.dashboard") : "" }}</v-btn>
-      <v-btn to="/sales" prepend-icon="mdi-cash-multiple">{{ navWithText ? $t("nav.sales") : "" }}</v-btn>
-      <v-btn to="/items" prepend-icon="mdi-package-variant-closed">{{ navWithText ? $t("nav.items") : "" }}</v-btn>
-      <v-btn to="/warehouse" prepend-icon="mdi-warehouse">{{ navWithText ? $t("nav.warehouse") : "" }}</v-btn>
-      <v-btn to="/cashregister" prepend-icon="mdi-cash-register">{{ navWithText ? $t("nav.cashregister") : "" }}</v-btn>
-      <v-btn to="/addressbook" prepend-icon="mdi-account-box-multiple">{{ navWithText ? $t("nav.addressbook") : "" }}</v-btn>
-      <v-btn to="/export" prepend-icon="mdi-file-export">{{ navWithText ? $t("nav.export") : "" }}</v-btn>
+      <v-btn to="/dashboard" prepend-icon="mdi-view-dashboard">{{ navWithText ? t("nav.dashboard") : "" }}</v-btn>
+      <v-btn to="/sales" prepend-icon="mdi-cash-multiple">{{ navWithText ? t("nav.sales") : "" }}</v-btn>
+      <v-btn to="/items" prepend-icon="mdi-package-variant-closed">{{ navWithText ? t("nav.items") : "" }}</v-btn>
+      <v-btn to="/warehouse" prepend-icon="mdi-warehouse">{{ navWithText ? t("nav.warehouse") : "" }}</v-btn>
+      <v-btn to="/cashregister" prepend-icon="mdi-cash-register">{{ navWithText ? t("nav.cashregister") : "" }}</v-btn>
+      <v-btn to="/addressbook" prepend-icon="mdi-account-box-multiple">{{ navWithText ? t("nav.addressbook") : "" }}</v-btn>
+      <v-btn to="/export" prepend-icon="mdi-file-export">{{ navWithText ? t("nav.export") : "" }}</v-btn>
     </div>
   </template>
 
@@ -29,30 +29,30 @@
       </v-avatar>
     </template>
     <v-list>
-      <v-list-item :to="'/account'" :prepend-icon="'mdi-account-cog'" :title="$t('nav.profile')"></v-list-item>
+      <v-list-item :to="'/account'" :prepend-icon="'mdi-account-cog'" :title="userName"></v-list-item>
       <v-divider></v-divider>
-      <v-list-item :to="'/settings'" :prepend-icon="'mdi-cog'" :title="$t('nav.settings')" />
-      <v-list-item :to="'/support'" link :prepend-icon="'mdi-lifebuoy'" :title="$t('nav.support')" />
+      <v-list-item :to="'/settings'" :prepend-icon="'mdi-cog'" :title="t('nav.settings')" />
+      <v-list-item :to="'/support'" link :prepend-icon="'mdi-lifebuoy'" :title="t('nav.support')" />
       <v-divider></v-divider>
-      <v-list-item @click="handleLogout" :prepend-icon="'mdi-logout'" :title="$t('nav.logout')" />
+      <v-list-item @click="handleLogout" :prepend-icon="'mdi-logout'" :title="t('nav.logout')" />
     </v-list>
   </v-menu>
 </v-app-bar>
 
   <v-navigation-drawer v-if="!display.lgAndUp.value || !navInAppBar" v-model="isDrawerOpen" :rail="display.lgAndUp.value && !navWithText" :temporary="!display.lgAndUp.value">
     <v-list class="d-flex flex-column"> <v-list-item-group>
-        <v-list-item :to="'/dashboard'" link :prepend-icon="'mdi-view-dashboard'" :title="$t('nav.dashboard')" />
-        <v-list-item :to="'/sales'" link :prepend-icon="'mdi-cash-multiple'" :title="$t('nav.sales')" />
-        <v-list-item :to="'/items'" link :prepend-icon="'mdi-package-variant-closed'" :title="$t('nav.items')" />
-        <v-list-item :to="'/warehouse'" link :prepend-icon="'mdi-warehouse'" :title="$t('nav.warehouse')" />
-        <v-list-item :to="'/cashregister'" link :prepend-icon="'mdi-cash-register'" :title="$t('nav.cashregister')" />
-        <v-list-item :to="'/addressbook'" link :prepend-icon="'mdi-account-box-multiple'" :title="$t('nav.addressbook')" />
-        <v-list-item :to="'/export'" link :prepend-icon="'mdi-file-export'" :title="$t('nav.export')" />
+        <v-list-item :to="'/dashboard'" link :prepend-icon="'mdi-view-dashboard'" :title="t('nav.dashboard')" />
+        <v-list-item :to="'/sales'" link :prepend-icon="'mdi-cash-multiple'" :title="t('nav.sales')" />
+        <v-list-item :to="'/items'" link :prepend-icon="'mdi-package-variant-closed'" :title="t('nav.items')" />
+        <v-list-item :to="'/warehouse'" link :prepend-icon="'mdi-warehouse'" :title="t('nav.warehouse')" />
+        <v-list-item :to="'/cashregister'" link :prepend-icon="'mdi-cash-register'" :title="t('nav.cashregister')" />
+        <v-list-item :to="'/addressbook'" link :prepend-icon="'mdi-account-box-multiple'" :title="t('nav.addressbook')" />
+        <v-list-item :to="'/export'" link :prepend-icon="'mdi-file-export'" :title="t('nav.export')" />
       </v-list-item-group>
       <v-divider :thickness="3"></v-divider>
       <v-list-item-group>
-        <v-list-item :to="'/settings'" link :prepend-icon="'mdi-cog'" :title="$t('nav.settings')" />
-        <v-list-item :to="'/support'" link :prepend-icon="'mdi-lifebuoy'" :title="$t('nav.support')" />
+        <v-list-item :to="'/settings'" link :prepend-icon="'mdi-cog'" :title="t('nav.settings')" />
+        <v-list-item :to="'/support'" link :prepend-icon="'mdi-lifebuoy'" :title="t('nav.support')" />
       </v-list-item-group>
     </v-list>
   </v-navigation-drawer>
@@ -68,11 +68,17 @@ import { useDisplay } from "vuetify";
 import { useAppStore } from "@/stores/app";
 import { storeToRefs } from "pinia";
 import { useRouter } from "vue-router";
+import { useI18n } from 'vue-i18n';
 
 const display = useDisplay();
 const appStore = useAppStore();
 const router = useRouter();
-const { navInAppBar, navWithText } = storeToRefs(appStore);
+
+// Inicializace t pro překlady
+const { t } = useI18n();
+
+// Získání refů z Pinia store, včetně userName
+const { navInAppBar, navWithText, userName } = storeToRefs(appStore); // <-- ZMĚNA ZDE
 const mobileDrawerOpen = ref(false);
 
 const isDrawerOpen = computed({
